9306da84fd72f92d06b4ec66832c09700ed2ed75
Facundo Mengoni <facundo.mengoni@globallogic.com>
2017-09-21 14:46:33 -0300

    Added remove key from shared preference

--- Library/futbol/src/main/java/com/globallogic/futbol/core/strategies/sharedpreference/RemoveSharedPreferenceStrategy.java
+++ Library/futbol/src/main/java/com/globallogic/futbol/core/strategies/sharedpreference/RemoveSharedPreferenceStrategy.java
@@ -1,8 +1,10 @@
 package com.globallogic.futbol.core.strategies.sharedpreference;
 
 import com.globallogic.futbol.core.interfaces.analyzers.IStrategySharedPreferenceAnalyzer;
-import com.globallogic.futbol.core.interfaces.repository.BooleanRepository;
+import com.globallogic.futbol.core.interfaces.repository.SharedPreferenceRepository;
 import com.globallogic.futbol.core.operations.Operation;
+import com.globallogic.futbol.core.responses.StrategySharedPreferenceResponse;
+import com.globallogic.futbol.core.strategies.SharedPreferenceStrategy;
 
 /**
  * A specific implementation to helps to clean a key from shared preferences.
@@ -10,14 +12,14 @@ import com.globallogic.futbol.core.operations.Operation;
  * @author facundo.mengoni
  * @since 0.3.8
  */
-public class RemoveSharedPreferenceStrategy extends GetSharedPreferenceStrategy<Boolean, BooleanRepository> {
-    public RemoveSharedPreferenceStrategy(Operation anOperation, IStrategySharedPreferenceAnalyzer anAnalyzer, BooleanRepository repository) {
+public class RemoveSharedPreferenceStrategy extends SharedPreferenceStrategy<Void, SharedPreferenceRepository> {
+    public RemoveSharedPreferenceStrategy(Operation anOperation, IStrategySharedPreferenceAnalyzer anAnalyzer, SharedPreferenceRepository repository) {
         super(anOperation, anAnalyzer, repository);
     }
 
     @Override
-    protected Boolean getResult(BooleanRepository repository) {
-        repository.removeKey();
-        return true;
+    protected void doRequestImpl() {
+        getRepository().removeKey();
+        parseResponse(null, new StrategySharedPreferenceResponse<Void>());
     }
 }
